6e66782e029086de549eb18521e0f7d22ad8baf6
Soares Ayoigbala <soaresayoigbala@gmail.com>
2025-06-25 17:53:03 +0100

    update

--- git.py
+++ git.py
@@ -3,3 +3,8 @@ laptop_brands = ['HP', 'Dell','Lenovo','Acer', 'Microsoft']
 
 for i in laptop_brands:
     print(i)
+
+user_input = input('What laptop brand?')
+laptop_brands.append(user_input)
+
+print(laptop_brands)
